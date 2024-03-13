1e787bd5eeb2a67b8a279d877126d58b9893db30
jonabergamo <jonathanbergamo16@gmail.com>
2024-03-13 13:47:16 -0300

    feat(commitizen): add git-cz script

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -8,6 +8,12 @@ export class User {
   @Prop({ unique: true, required: true })
   username: string;
 
+  @Prop({ unique: true, required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password?: string;
+
   @Prop({ required: false })
   displayName?: string;
 
